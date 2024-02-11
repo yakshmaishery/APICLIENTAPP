a57aa2b7ca040ae883bbdf8112eaaf18586e21db
yakshmaishery <yakshmaishery@users.noreply.github.com>
2024-02-11 14:08:57 +0530

    New Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,14 +52,19 @@
     let HeaderKey = ""
     let Headeralue = ""
     let HeaderBody = {}
+    let GetParams = {}
+    let paramkey = ""
+    let paramvalue = ""
     let Postbodytext = ""
     let Alertbox = false;
-    let BLURL = "https://apiserver-roan.vercel.app/"
+    let warnbox = false;
+    let BLURL = "https://apiserver-roan.vercel.app/"  // API Calling URL
     // @ts-ignore
     const changeMethod = (e) => {
         APIMethod = e.target.innerHTML
     }
 
+    // validation for the JSON format
     const isValidJSON = obj => {
       try {
         JSON.parse(obj);
@@ -69,24 +74,41 @@
       }
     };
 
+    // Add the Key value pair from the POST Method
     const AddKeyValuePair = () => {
         APIBody[bodyKey] = bodyvalue
     }
 
+    // Remove the Key value pair from the POST Method
     const RemoveKeyValuePair = () => {
         delete(APIBody[bodyKey]);
         APIBody = APIBody
     }
 
+    // Add the Key value pair from the Headers
     const AddKeyValuePairHeader = () => {
       HeaderBody[HeaderKey] = Headeralue
     }
 
+    // Remove the Key value pair from the Headers
     const RemoveKeyValuePairHeader = () => {
         delete(HeaderBody[HeaderKey]);
         HeaderBody = HeaderBody
     }
+    
+    // Add the Key value pair from the GET Method
+    const AddKeyValuePairQueryParam = () => {
+      GetParams[paramkey] = paramvalue
+      changeParamValue(paramkey,paramvalue)
+    }
 
+    // Remove the Key value pair from the GET Method
+    const RemoveKeyValuePairQueryParam = () => {
+        delete(GetParams[paramkey]);
+        GetParams = GetParams
+    }
+
+    // Change in the Textarea for the POST
     const ChangesPostBody = () => {
       try
       {
@@ -98,6 +120,7 @@
       }
     }
 
+    // Clear button Logic
     const ClearFunction = () => {
       APIMethod = "GET";
       APIURL = "";
@@ -112,8 +135,10 @@
       HeaderKey = ""
       Headeralue = ""
       HeaderBody = {}
+      GetParams = {}
     }
 
+    // Send Button Logic
     const sendFunc = async () => {
       Loader=true;
       document.getElementById("pills-contact-tab").click()
@@ -213,14 +238,26 @@
       Loader=false;
     }
 
+    // Download the API documentation
     const DownLoadFunc = async () => {
       const response = await fetch('DownloadTempate.txt');
       let fileData = await response.text()
-      fileData = fileData.replace("[docs]","API Documentation as on "+new Date().toLocaleString())
-      fileData = fileData.replace("[URL]",APIURL)
-      fileData = fileData.replace("[method]",APIMethod)
-      fileData = fileData.replace("[RequestHeaders]",JSON.stringify(HeaderBody,null,3))
-      fileData = fileData.replace("[RequestBody]",JSON.stringify(APIBody,null,3))
+      fileData = fileData.replace("[docs]","API Documentation as on "+new Date().toLocaleString()) // Add the First heading and as on Date
+      fileData = fileData.replace("[URL]",APIURL) // URL 
+      fileData = fileData.replace("[method]",APIMethod) // METHOD of API :- GET,POST
+      fileData = fileData.replace("[RequestHeaders]",JSON.stringify(HeaderBody,null,3)) // Request Headers
+
+      // Request Body depending on GET and POST
+      if(APIMethod == "GET")
+      {
+        fileData = fileData.replace("[RequestBody]",JSON.stringify(GetParams,null,3))
+      }
+      else if(APIMethod == "POST")
+      {
+        fileData = fileData.replace("[RequestBody]",JSON.stringify(APIBody,null,3))
+      }
+      
+      // Response Headers
       if(isValidJSON(ResponseHeaders))
       {
         fileData = fileData.replace("[ResponseHeaders]",JSON.stringify(JSON.parse(ResponseHeaders),null,3))
@@ -228,6 +265,8 @@
       else{
         fileData = fileData.replace("[ResponseHeaders]",JSON.stringify(ResponseHeaders,null,3))
       }
+
+      // Response Body
       if(isValidJSON(ResponseData))
       {
         fileData = fileData.replace("[ResponseBody]",JSON.stringify(JSON.parse(ResponseData),null,3))
@@ -235,16 +274,45 @@
       else{
         fileData = fileData.replace("[ResponseBody]",JSON.stringify(JSON.parse(ResponseData),null,3))
       }
+
+      // Create a Text file Blob
       var blob = new Blob([fileData],  
                 {type : "text/plain"});
       var url = window.URL || window.webkitURL;
       let link = url.createObjectURL(blob);
       let a = document.createElement("a");
-      a.setAttribute("download", `APIDocs.txt`);
+      a.setAttribute("download", `APIDocs.txt`); // Set the Default File Name for the document
       a.setAttribute("href", link);
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
+    }
+
+    // Change in URL
+    const changeURLFunc = () => {
+      if(APIMethod == "GET"){
+        let queryurl = new URL(APIURL)
+        let params = new URLSearchParams(queryurl.search)
+        GetParams = {}
+        for(var key of params.keys()){
+          GetParams[key] = params.get(key)
+        }
+      }
+    }
+
+    // change params value
+    const changeParamValue = (key,value) => {
+      GetParams[key] = value
+      let params = GetParams;
+      // convert object to list -- to enable .map
+      let data = Object.entries(params);
+      // encode every parameter (unpack list into 2 variables)
+      data = data.map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`);
+      // combine into string
+      let query = data.join('&');
+      let newURL = new URL(APIURL)
+      newURL.search = query
+      APIURL = newURL.href
     }
 </script>
 <HeaderComponent />
@@ -254,6 +322,14 @@
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={() => {Alertbox = false}}></button>
   </div>
   {/if}
+  {#if warnbox}
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Are you Sure? you want to reset</strong>
+    <button class="btn btn-success" on:click={() => {ClearFunction();warnbox=false}}>Yes</button>
+    <button class="btn btn-success" on:click={() => {warnbox = false}}>No</button>
+    <!-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" on:click={() => {Alertbox = false}}></button> -->
+  </div>
+  {/if}
 <div class="container-fluid">
     <div class="input-group mb-3" style="padding-top:10px">
         <button class="btn btn-success fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{APIMethod}</button>
@@ -261,11 +337,11 @@
           <li><button class="dropdown-item fw-bold" on:click={changeMethod}>GET</button></li>
           <li><button class="dropdown-item fw-bold" on:click={changeMethod}>POST</button></li>
         </ul>
-        <input type="text" class="form-control" placeholder="Enter URL..." bind:value={APIURL} aria-label="Text input with dropdown button">
+        <input type="text" class="form-control" placeholder="Enter URL..." bind:value={APIURL} on:change={changeURLFunc} aria-label="Text input with dropdown button">
         <button type="button" class="btn btn-outline-success" on:click={sendFunc}>SEND <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
           <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
         </svg></button>
-        <button class="btn btn-outline-success" on:click={ClearFunction}>Clear <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+        <button class="btn btn-outline-success" on:click={() => {warnbox = true}}>Clear <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
         </svg></button>
       </div>
@@ -337,7 +413,29 @@
               </div>
               {:else}
               <div>
-                <h6 class="responsesstyles">Only for the POST Request Body is accessable</h6>
+                <!-- <h6 class="responsesstyles">Only for the POST Request Body is accessable</h6> -->
+                <!-- <pre class="responsesstyles">{JSON.stringify(GetParams,null,3)}</pre> -->
+                <div class="row">
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Param Key" disabled={APIURL==""?true:false} bind:value={paramkey} aria-label="First name">
+                  </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Param Value" disabled={APIURL==""?true:false} bind:value={paramvalue} aria-label="Last name">
+                  </div>
+                  <div class="col">
+                    <button class="btn btn-success" on:click={AddKeyValuePairQueryParam}>➕</button>
+                    <button class="btn btn-success" on:click={RemoveKeyValuePairQueryParam}>➖</button>
+                  </div>
+                </div>
+                <br>
+                <div class="row">
+                {#each Object.entries(GetParams) as [keypar,valuepar]}
+                <div class="input-group mb-3">
+                  <span class="input-group-text fw-bold" id="inputGroup-sizing-default">{keypar}</span>
+                  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value={valuepar} on:input={(e) => changeParamValue(keypar,e.target.value)}>
+                </div>
+                {/each}
+              </div>
               </div>
             {/if}
         </div>
